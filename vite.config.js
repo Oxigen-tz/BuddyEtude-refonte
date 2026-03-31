118c031f03222975585b59c94b6996be21e121f5
Oxigen-tz <bertreux.charles@gmail.com>
2026-03-31 16:23:10 +0200

    fix : bug

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
@@ -14,27 +13,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    // On désactive les logs inutiles de Base44
+    // On désactive les logs inutiles
     strictPort: false,
   }
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-
-// Configuration standard pour React + Vite
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // C'est ce qui permet d'utiliser le "@" dans tes imports
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    port: 5173,
-    // On désactive les logs inutiles de Base44
-    strictPort: false,
-  }
->>>>>>> 6b88a12f674c32552cb3079a33c97e7810e3fe29
 })
